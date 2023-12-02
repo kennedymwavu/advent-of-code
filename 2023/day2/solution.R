4d1fa7d2f26c74c29250dd025d805540381a01ea
kennedymwavu <mwavukennedy@gmail.com>
2023-12-03 00:28:00 +0300

    ft: add part 2 of the solution

--- 2023/day2/solution.R
+++ 2023/day2/solution.R
@@ -19,3 +19,22 @@ is_possible <- \(n_red, n_green, n_blue) {
 inds <- Map(f = is_possible, n_reds, n_greens, n_blues) |> unlist()
 
 seq_along(input)[inds] |> sum()
+
+# part 2----
+
+convert_empty_to_zero <- \(num) if (length(num) < 1L) 0 else num
+
+get_least_cubes <- \(input, color) {
+  pattern <- sprintf("(\\d+)(?=\\s%s)", color)
+  input |>
+    stringr::str_extract_all(pattern = pattern) |>
+    lapply(as.numeric) |>
+    lapply(convert_empty_to_zero) |>
+    lapply(max)
+}
+
+n_reds <- get_least_cubes(input, "red")
+n_greens <- get_least_cubes(input, "green")
+n_blues <- get_least_cubes(input, "blue")
+
+Map(f = prod, n_reds, n_greens, n_blues) |> Reduce(f = `+`)

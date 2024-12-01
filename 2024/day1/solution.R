9ede33f445883ee13356413ad17c52344287da46
Kennedy Mwavu <mwavukennedy@gmail.com>
2024-12-02 00:20:31 +0300

    ft: solve day 1 part 2

--- 2024/day1/solution.R
+++ 2024/day1/solution.R
@@ -8,3 +8,9 @@ input <- read.table(
 
 dist <- abs(input$group1 - input$group2) |> sum()
 dist
+
+# part 2:
+g1 <- input$group1[input$group1 %in% input$group2] |> unique()
+g2 <- input$group2[input$group2 %in% g1]
+n <- sapply(g1, \(i) sum(i == g2))
+sum(g1 * n)
